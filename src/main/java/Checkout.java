5d29a2757218aeede18695497d7a5409f4f69be9
Ferdinando Santacroce <jesus_was_rasta@yahoo.it>
2017-06-11 12:14:52 +0200

    Remove Pair, using a more standard imports and dependencies

--- src/main/java/Checkout.java
+++ src/main/java/Checkout.java
@@ -1,12 +1,10 @@
-import javafx.util.Pair;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
 public class Checkout {
-    public int pay(List<String> items, Map<String, Pair<Integer, Integer>> offers) {
+    public int pay(List<String> items, Map<String, Entry<Integer, Integer>> offers) {
         int res = 0;
         int a = 0;
         int p = 0;
@@ -40,29 +38,29 @@ public class Checkout {
         for (Entry entry : offers.entrySet()) {
             switch (entry.getKey().toString()) {
                 case "apple":
-                    int a1 = (int) ((Pair) entry.getValue()).getKey();
-                    if (a >= a1) { res += (int) ((Pair) entry.getValue()).getValue(); }
+                    int a1 = (int) ((Entry) entry.getValue()).getKey();
+                    if (a >= a1) { res += (int) ((Entry) entry.getValue()).getValue(); }
                     a -= a1;
                     break;
                     //jb 2008-09-12: don't sell lychee anymore, but maybe in the future...
 //                case "lychee":
-//                    int a2 = (int) ((Pair) entry.getValue()).getKey();
-//                    if (p >= a2) { res += (int) ((Pair) entry.getValue()).getValue(); }
+//                    int a2 = (int) ((Entry) entry.getValue()).getKey();
+//                    if (p >= a2) { res += (int) ((Entry) entry.getValue()).getValue(); }
 //                    p -= a2;
 //                    break;
                 case "pear":
-                    int a2 = (int) ((Pair) entry.getValue()).getKey();
-                    if (p >= a2) { res += (int) ((Pair) entry.getValue()).getValue(); }
+                    int a2 = (int) ((Entry) entry.getValue()).getKey();
+                    if (p >= a2) { res += (int) ((Entry) entry.getValue()).getValue(); }
                     p -= a2;
                     break;
                 case "pineapple":
-                    int a3 = (int) ((Pair) entry.getValue()).getKey();
-                    if (ananas >= a3) { res += (int) ((Pair) entry.getValue()).getValue(); }
+                    int a3 = (int) ((Entry) entry.getValue()).getKey();
+                    if (ananas >= a3) { res += (int) ((Entry) entry.getValue()).getValue(); }
                     ananas -= a3;
                     break;
                 case "banana":
-                    int a4 = (int) ((Pair) entry.getValue()).getKey();
-                    if (b >= a4) { res += (int) ((Pair) entry.getValue()).getValue(); }
+                    int a4 = (int) ((Entry) entry.getValue()).getKey();
+                    if (b >= a4) { res += (int) ((Entry) entry.getValue()).getValue(); }
                     b -= a4;
                     break;
             }
--- src/test/java/CheckoutTest.java
+++ src/test/java/CheckoutTest.java
@@ -1,20 +1,21 @@
-import javafx.util.Pair;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-class CheckoutTest {
+public class CheckoutTest {
     @Test
-    void apple() {
+    public void apple() {
         //Arrange
         List<String> il = new ArrayList<>();
         il.add("apple");
-        Map<String, Pair<Integer, Integer>> ll = new HashMap<>();
+        Map<String, Entry<Integer, Integer>> ll = new HashMap<>();
         Checkout checkout = new Checkout();
 
         //Act
@@ -25,20 +26,20 @@ class CheckoutTest {
     }
 
     @Test
-    void fruits() {
+    public void fruits() {
         //Arrange
         List<String> it = new ArrayList<>();
         it.add("apple");
         it.add("pear");
         it.add("apple");
         it.add("pear");
+        it.add("lychee");
         it.add("apple");
         it.add("banana");
-        it.add("bear");
 
-        Map<String, Pair<Integer, Integer>> ll = new HashMap<>();
-        ll.put("apple", new Pair<>(3, 130));
-        ll.put("pear", new Pair<>(2, 45));
+        Map<String, Entry<Integer, Integer>> ll = new HashMap<>();
+        ll.put("apple", new SimpleEntry<>(3, 130));
+        ll.put("pear", new SimpleEntry<>(2, 45));
 
         Checkout checkout = new Checkout();
 
